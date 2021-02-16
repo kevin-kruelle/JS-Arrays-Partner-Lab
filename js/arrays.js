ba2516b8092677feb15f13e313aae1f21f7dc937
Kevin Kruelle <kkruelle2@gmail.com>
2021-02-16 14:59:13 -0500

    finished part 1 problem 1

--- js/arrays.js
+++ js/arrays.js
@@ -1,1 +1,12 @@
 console.log('connected...')
+
+let myArray = [2,3,4,5,6]
+
+const empty = function(arr) {
+    if(myArray === undefined || myArray.length == 0) {
+        console.log('Array is empty')
+    } else {
+        console.log('Array is not empty')
+    }
+}
+empty(myArray);
