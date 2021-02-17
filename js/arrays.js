7e7bb6f6b4929398a39b97e192c0e6bea1224b28
Michael Leonhard <77648651+MSLeonhard@users.noreply.github.com>
2021-02-17 15:56:02 -0500

    completed part 2 and step 1 of part 3

--- js/arrays.js
+++ js/arrays.js
@@ -45,12 +45,34 @@ console.log(removeElm(myArray))
 let alpha = [2,3,4,5,6,]
 let beta = [14,24,34,36,34]
 
-const concat = (arr1, arr2) => {
+const copy = (arr1, arr2) => {
     let copyValues = arr2.push(...arr1)
     return arr2 
 }
 
-console.log(concat(alpha, beta))
+//console.log(copy(alpha, beta))
 
 
+//Step 3
 
+const add = (arr, index, value) => {
+    let newArray = arr.splice(index, 0, value);
+    return arr
+}
+
+//console.log(add(alpha, 2, 20))
+
+//Part 3
+//Step 1
+
+const move = (arr1, arr2) => {
+    let newArray = arr1.splice(0, 0, ...arr2 );
+    return arr1
+}
+
+console.log(move(beta, alpha))
+
+//Step 2
+const swap = (arr, a, b) => {
+    
+}
